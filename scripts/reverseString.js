6068ee098b503f706abe4ad80b7cc47151cd2993
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-08-13 21:25:41 -0700

    main reverse string functionality

--- scripts/reverseString.js
+++ scripts/reverseString.js
@@ -1,3 +1,4 @@
 export default function reverseString(inputString) {
     if (typeof inputString !== 'string') throw new Error();
+    return inputString.split('').reverse().join('');
 }
--- scripts/tests/reverseString.test.js
+++ scripts/tests/reverseString.test.js
@@ -4,3 +4,14 @@ test('Non-string input should result in an error', () => {
     expect(() => { reverseString(5); }).toThrow();
 });
 
+test('Empty string input should output an empty string', () => {
+    expect(reverseString('')).toMatch('');
+});
+
+test('Single character string input should output same string', () => {
+    expect(reverseString('a')).toMatch('a');
+});
+
+test('Multi-character string input should output that string reversed', () => {
+    expect(reverseString('bCdEF')).toMatch('FEdCb');
+})
